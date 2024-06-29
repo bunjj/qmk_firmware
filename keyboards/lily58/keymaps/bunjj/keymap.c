be52436bb78b638637c8c6cdd02b3914abcd9668
Irfan Bunjaku <irfan_bunjaku@hotmail.com>
2024-06-29 17:53:51 +0200

    add layout for ergodash

--- keyboards/lily58/keymaps/bunjj/keymap.c
+++ keyboards/lily58/keymaps/bunjj/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  LALT_T(KC_TAB),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, RALT_T(KC_MINS),
  LCTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_KB_VOLUME_DOWN,  KC_KB_VOLUME_UP,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), LSFT_T(KC_SPC), RSFT_T(KC_ENT), MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), LSFT_T(KC_SPC), RSFT_T(KC_ENT), MO(_RAISE), KC_BSPC, KC_LGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
